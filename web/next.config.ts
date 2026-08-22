db1732e2ae849132c461de4e5e80491ef8ce63cc
Tomas-programmer <tomasgodefamerha@gmail.com>
2026-08-23 01:02:08 +0300

    fetch real data from Api

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
   async rewrites() {
     return [
       {
@@ -12,3 +16,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
